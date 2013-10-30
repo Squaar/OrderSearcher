0d5aab41725efd99b2208cc44e90fcf282fc3a41
Matt Dumford <mdumford99@gmail.com>
2013-10-29 19:21:48 -0500

    downloaded files, can read files, bytes are weird

--- orderSearcher.c
+++ orderSearcher.c
@@ -14,9 +14,39 @@ int main(int argc, char **argv){
 	char *file = argv[1];
 	int numThreads = strtol(argv[2], 0, 10);
 
-	printf("File: %s, numThreads: %i\n", file, numThreads);
+	if(numThreads < 1){
+		printf("Must have at least one thread.");
+		exit(-1);
+	}
 
+	//open file
+	FILE *fd = fopen(file, "rb");
+	if(fd == NULL){
+		perror("Error opening file");
+		exit(-1);
+	}
 
+	//get size of file
+	fseek(fd, 0L, SEEK_END);
+	long size = ftell(fd);
+	fseek(fd, 0L, SEEK_SET);
+
+	//read in from file
+	char *inBuffer = malloc(size);
+	size_t readBytes = fread(inBuffer, 1, size, fd);
+	if(readBytes != size){
+		printf("Error reading from file");
+		exit(-1);
+	}
+
+	//close file
+	fclose(fd);
+
+	printf("%s\n", inBuffer);
+	int i;
+	for(i=0; i<size; i++){
+		printf("0x%X\n", inBuffer[i]);
+	}
 
 	return 0; 
 }
